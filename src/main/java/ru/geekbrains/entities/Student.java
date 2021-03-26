16d9773e241bc49e5626eb4f8820bd847d514c05
listpv <listpv@mail.ru>
2021-03-26 14:26:16 +0300

    Выполнено ДЗ№7, корректировка.

--- src/main/java/ru/geekbrains/entities/Student.java
+++ src/main/java/ru/geekbrains/entities/Student.java
@@ -20,27 +20,4 @@ public class Student {
     @Column(name = "age")
     private Integer age;
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public Integer getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Integer age) {
-//        this.age = age;
-//    }
 }
